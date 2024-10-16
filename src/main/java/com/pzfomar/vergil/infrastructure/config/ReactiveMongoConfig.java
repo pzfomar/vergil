abb704fc433bb0575f2548e5cffddec5e0f2c1ba
pzfomar <fredyomarpz@icloud.com>
2024-10-15 22:33:05 -0600

    ✨ Add security

--- src/main/java/com/pzfomar/vergil/infrastructure/config/ReactiveMongoConfig.java
+++ src/main/java/com/pzfomar/vergil/infrastructure/config/ReactiveMongoConfig.java
@@ -1,19 +1,27 @@
 package com.pzfomar.vergil.infrastructure.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 import com.mongodb.reactivestreams.client.MongoClient;
 
 @Configuration
+@EnableReactiveMongoRepositories
+@EnableReactiveMongoAuditing
 public class ReactiveMongoConfig {
+    @Value("${spring.data.mongodb.database:vergil}")
+    private String springDataMongodbDatabase;
+
     @Autowired
     private MongoClient mongoClient;
 
     @Bean
     public ReactiveMongoTemplate reactiveMongoTemplate() {
-        return new ReactiveMongoTemplate(mongoClient, "test");
+        return new ReactiveMongoTemplate(mongoClient, springDataMongodbDatabase);
     }
 }
---
+++ src/main/java/com/pzfomar/vergil/infrastructure/config/security/JwtTokenProvider.java
@@ -0,0 +1,86 @@
+package com.pzfomar.vergil.infrastructure.config.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Collection;
+import java.util.Date;
+import javax.crypto.SecretKey;
+
+import static java.util.stream.Collectors.joining;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+    @Value("${jwt.secretKey:rzxlszyykpbgqcflzxsqcysyhljt}")
+    private String jwtSecretKey;
+
+    @Value("${jwt.validityInMs:3600000}")
+    private long jwtValidityInMs;
+
+    private static final String AUTHORITIES_KEY = "roles";
+
+    //private final JwtProperties jwtProperties;
+
+    private SecretKey secretKey;
+
+    @PostConstruct
+    public void init() {
+        var secret = Base64.getEncoder().encodeToString(this.jwtSecretKey.getBytes());
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String createToken(Authentication authentication) {
+        String username = authentication.getName();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        var claimsBuilder = Jwts.claims().subject(username);
+        if (!authorities.isEmpty()) {
+            claimsBuilder.add(AUTHORITIES_KEY,
+                    authorities.stream().map(GrantedAuthority::getAuthority).collect(joining(",")));
+        }
+        var claims = claimsBuilder.build();
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + this.jwtValidityInMs);
+        return Jwts.builder().claims(claims).issuedAt(now).expiration(validity).signWith(this.secretKey, Jwts.SIG.HS256)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts.parser().verifyWith(this.secretKey).build().parseSignedClaims(token).getPayload();
+        Object authoritiesClaim = claims.get(AUTHORITIES_KEY);
+        Collection<? extends GrantedAuthority> authorities = authoritiesClaim == null ? AuthorityUtils.NO_AUTHORITIES
+                : AuthorityUtils.commaSeparatedStringToAuthorityList(authoritiesClaim.toString());
+        User principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().verifyWith(this.secretKey).build().parseSignedClaims(token);
+            log.info("expiration date: {}", claims.getPayload().getExpiration());
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            log.info("Invalid JWT token: {}", e.getMessage());
+            log.trace("Invalid JWT token trace.", e);
+        }
+        return false;
+    }
+
+}
